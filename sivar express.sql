c465b9ee8512b2bea4faad90022d473cc656f02d
Arielgomezcartagena <202400545@ujmd.edu.sv>
2026-08-12 18:55:06 -0600

    Initial commit: estructura CI/CD y migration inicial Sivar Express

--- sivar express.sql
+++ sivar express.sql
@@ -137,24 +137,3 @@ INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad, precio_unitario_hi
 (1001, 'PROD-08', 2, 2.00),
 (1002, 'PROD-01', 6, 1.25),
 (1002, 'PROD-12', 2, 2.50);
-
-SELECT
-    pe.id_pedido,
-    c.nombre AS cliente,
-    c.direccion_linea1,
-    r.nombre AS repartidor,
-    dp.cantidad,
-    pr.nombre AS producto,
-    dp.precio_unitario_historico,
-    pe.costo_envio,
-    (dp.cantidad * dp.precio_unitario_historico) AS subtotal,
-    (dp.cantidad * dp.precio_unitario_historico + pe.costo_envio) AS total_cobrado
-FROM pedido pe
-JOIN cliente c
-    ON pe.dui_cliente = c.dui_cliente
-JOIN repartidor r
-    ON pe.id_repartidor = r.id_repartidor
-JOIN detalle_pedido dp
-    ON pe.id_pedido = dp.id_pedido
-JOIN producto pr
-    ON dp.id_producto = pr.id_producto;
